5389bc6447e64efde020c3c02a3a81391e63745e
Yury Trembach <yury.trembach@gmail.com>
2024-09-22 20:11:33 +0300

    added
    - redis init
    - processing data for Redis
    - comparison Mysql vs REdis

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,39 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import dao.CityDAO;
 import dao.CountryDAO;
 import domain.City;
 import domain.Country;
 import domain.Language;
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.RedisURI;
+import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.api.sync.RedisStringCommands;
 import lombok.SneakyThrows;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+import redis.CityCountry;
+import tests.TestMysql;
+import tests.TestRedis;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Properties;
+import java.util.*;
+import java.util.stream.Collectors;
 
 import static java.util.Objects.nonNull;
 
 public class Main {
     final private SessionFactory sessionFactory;
+    final private RedisClient redisClient;
+    final ObjectMapper mapper;
 
     final private CountryDAO countryDAO;
     final private CityDAO cityDAO;
 
     public Main() {
         sessionFactory = prepareMysql();
+        redisClient = prepareRedis();
+        mapper = new ObjectMapper();
 
         cityDAO = new CityDAO(sessionFactory);
         countryDAO = new CountryDAO(sessionFactory);
@@ -29,8 +41,8 @@ public class Main {
 
     @SneakyThrows
     private SessionFactory prepareMysql() {
-        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-        Properties properties = new Properties();
+        final ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+        final Properties properties = new Properties();
         properties.load(classLoader.getResourceAsStream("mysql.properties"));
 
         return new Configuration()
@@ -41,9 +53,20 @@ public class Main {
                 .buildSessionFactory();
     }
 
+    private RedisClient prepareRedis() {
+        final RedisClient redisClient = RedisClient.create(RedisURI.create("localhost", 6379));
+        try (StatefulRedisConnection<String, String> connection = redisClient.connect()){
+            return redisClient;
+        }
+    }
+
     private void shutdown() {
         if (nonNull(sessionFactory)) {
             sessionFactory.close();
+        }
+
+        if (nonNull(redisClient)) {
+            redisClient.shutdown();
         }
     }
 
@@ -62,9 +85,45 @@ public class Main {
         return cities;
     }
 
+    private List<CityCountry> prepareCityCountry(final List<City> cities) {
+        return cities.stream()
+                .map(Converters::convertCityCountry)
+                .collect(Collectors.toList());
+    }
+
+    private void saveToRedis(final List<CityCountry> cityCountryList) {
+        try (StatefulRedisConnection<String, String> connection = redisClient.connect()) {
+            RedisStringCommands<String, String> sync = connection.sync();
+            for (final CityCountry cityCountry : cityCountryList) {
+                try {
+                    sync.set(String.valueOf(cityCountry.getId()), mapper.writeValueAsString(cityCountry));
+                } catch (JsonProcessingException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        Main main = new Main();
-        List<City> all = main.fetchData();
+        final Main main = new Main();
+        final List<City> allCities = main.fetchData();
+        final List<CityCountry> allCityCountries = main.prepareCityCountry(allCities);
+        main.saveToRedis(allCityCountries);
+
+        main.sessionFactory.getCurrentSession().close();
+        final List<Integer> ids = List.of (12, 56, 76, 44, 88, 123, 2123, 888, 1345, 999);
+
+        final long startRedis = System.currentTimeMillis();
+        new TestRedis(main.redisClient, main.mapper).testRedisData(ids);
+        final long stopRedis = System.currentTimeMillis();
+
+        final long startMysql = System.currentTimeMillis();
+        new TestMysql(main.sessionFactory, main.cityDAO).testMysqlData(ids);
+        final long stopMysql = System.currentTimeMillis();
+
+        System.out.printf("%s:\t%d ms\n", "Redis", (stopRedis - startRedis));
+        System.out.printf("%s:\t%d ms\n", "MySQL", (stopMysql - startMysql));
+
         main.shutdown();
     }
 }
