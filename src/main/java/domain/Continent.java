ba8542d46720af959dbb41a94bb8926075398cb5
Yury Trembach <yury.trembach@gmail.com>
2024-09-22 16:25:24 +0300

    added entities

---
+++ src/main/java/domain/Continent.java
@@ -0,0 +1,11 @@
+package domain;
+
+public enum Continent {
+    ASIA,
+    EUROPE,
+    NORTH_AMERICA,
+    AFRICA,
+    OCEANIA,
+    ANTARCTICA,
+    SOUTH_AMERICA
+}
---
+++ src/main/java/domain/Country.java
@@ -0,0 +1,62 @@
+package domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.math.BigDecimal;
+import java.util.Set;
+
+@Entity
+@Table(schema = "world", name = "country")
+@Getter
+@Setter
+public class Country {
+    @Id
+    private Integer id;
+
+    private String code;
+
+    @Column(name = "code_2")
+    private String alternativeCode;
+
+    private String name;
+
+    @Enumerated(EnumType.ORDINAL)
+    private Continent continent;
+
+    private String region;
+
+    @Column(name = "surface_area")
+    private BigDecimal surfaceArea;
+
+    @Column(name = "indep_year")
+    private Short independenceYear;
+
+    private Integer population;
+
+    @Column(name = "life_expectancy")
+    private BigDecimal lifeExpectancy;
+
+    private BigDecimal GNP;
+
+    @Column(name = "gnpo_id")
+    private BigDecimal GNPOId;
+
+    @Column(name = "local_name")
+    private String localName;
+
+    @Column(name = "government_form")
+    private String governmentForm;
+
+    @Column(name = "head_of_state")
+    private String getOfState;
+
+    @OneToOne (fetch = FetchType.LAZY)
+    @JoinColumn(name = "capital")
+    private City capital;
+
+    @OneToMany(fetch = FetchType.EAGER)
+    @JoinColumn(name = "country_id")
+    private Set<CountryLanguage> languages;
+}
